74c036a3432e36766ebbd03b918ae961f64a46e4
Igor Mariano <igor-mariano@outlook.com>
2022-10-14 08:10:46 -0300

    Atribuindo valores de outras variáveis

--- FirstSteps/src/firstSteps.java
+++ FirstSteps/src/firstSteps.java
@@ -1,9 +1,10 @@
 public class firstSteps {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        
+
         int myFirstNumber = (10 + 5) + (2 * 10);
         int mySecondNunmber = 12;
         int myThirdNmber = 6;
+        int myTotal = myFirstNumber + mySecondNunmber + myThirdNmber;
     }
 }
