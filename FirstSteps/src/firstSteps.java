320759fee007526839c53771e6df79ad3a99f8e5
Igor Mariano <igor-mariano@outlook.com>
2022-10-14 08:09:29 -0300

    Criando variáveis do tipo int

--- FirstSteps/src/firstSteps.java
+++ FirstSteps/src/firstSteps.java
@@ -1,5 +1,9 @@
 public class firstSteps {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNunmber = 12;
+        int myThirdNmber = 6;
     }
 }
