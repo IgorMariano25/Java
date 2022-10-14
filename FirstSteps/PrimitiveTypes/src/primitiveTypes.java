5941c4c2a432d2c2c9d32643ac7af7ce814f5add
Igor Mariano <igor-mariano@outlook.com>
2022-10-14 09:05:18 -0300

    Adicionando comentário no código

--- FirstSteps/PrimitiveTypes/src/primitiveTypes.java
+++ FirstSteps/PrimitiveTypes/src/primitiveTypes.java
@@ -3,6 +3,7 @@ public class primitiveTypes {
         System.out.println("Hello, World!");
 
         int myValue = 1000;
+        // Determining the minimum range of the int number
         int myMinimunValue = Integer.MIN_VALUE;
     }
 }
