dd613b437ca4552454bf570dd39c0bcd090ebd60
Igor Mariano <igor-mariano@outlook.com>
2022-10-14 08:14:56 -0300

    Print do valor da variável myTotal

--- FirstSteps/src/firstSteps.java
+++ FirstSteps/src/firstSteps.java
@@ -10,5 +10,6 @@ public class firstSteps {
         System.out.println("This is my first number: " + myFirstNumber);
         System.out.println("This is my second number: " + mySecondNunmber);
         System.out.println("This is my third number: " + myThirdNmber);
+        System.out.println("This is the sum of that numbers:" +  myTotal);
     }
 }
