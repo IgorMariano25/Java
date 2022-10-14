9ff8a64a633dd806e82ccf0114144b6e6a412e02
Igor Mariano <igor-mariano@outlook.com>
2022-10-14 09:20:14 -0300

    Traduzindo o comenário

--- FirstSteps/PrimitiveTypes/src/primitiveTypes.java
+++ FirstSteps/PrimitiveTypes/src/primitiveTypes.java
@@ -5,7 +5,7 @@ public class primitiveTypes {
         int myValue = 1000;
         // Determining the minimum range of the int number
         int myMinimumValue = Integer.MIN_VALUE;
-        // Determinando o intervalo máximo do número int
+        // Determining the maximum range of int number
         int myMaximumValue = Integer.MAX_VALUE;
         System.out.println("Integer Minimum value =" + myMinimumValue);
         System.out.println("Integer Maximum value = " + myMaximumValue);
