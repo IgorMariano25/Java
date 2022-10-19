ffea2a2401b09ba7918ee2dfa6137e06bad2a0ea
Igor Mariano <igor-mariano@outlook.com>
2022-10-19 13:23:08 -0300

    chellenge parte 2

--- PrimitiveTypes/src/challenge.java
+++ PrimitiveTypes/src/challenge.java
@@ -1,6 +1,9 @@
 /*
  Your challenge is to create a byte variable and set it to any valid byte number, id doesn't matter.
- Create a short variable and set it to any valid short number
+ Create a short variable and set it to any valid short number.
+
+ Crate an int variable ans set it to any valid int number lastly, create a variable of type long and
+ make it equal to 50000 plus 10 times the sum of the byte plus the short plus the integer values
  */
 public class challenge {
     public static void main(String[] args) throws Exception {
