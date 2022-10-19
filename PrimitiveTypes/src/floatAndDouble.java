95f12872eb86286fa87f725da497fda0b0b02988
Igor Mariano <igor-mariano@outlook.com>
2022-10-19 13:54:02 -0300

    Adicionando cabeçalho para o float

--- PrimitiveTypes/src/floatAndDouble.java
+++ PrimitiveTypes/src/floatAndDouble.java
@@ -1,9 +1,14 @@
 public class floatAndDouble {
     public static void main(String[] args) throws Exception {
-    float myMinimunFloatValue = Float.MIN_VALUE;
-    float myMaximunFloatValue = Float.MAX_VALUE;
-    System.out.println("The minimun value of a float number is equal to: " + myMinimunFloatValue);
-    System.out.println("The maximun value of a float number is equal to: " + myMaximunFloatValue);
+    System.out.println("===============================================");
+    System.out.println("  MINIMUN AND MAXIMUM VALUE OF A FLOAT NUMBER  ");
+    System.out.println("===============================================");
+    float myMinimumFloatValue = Float.MIN_VALUE;
+    float myMaximumFloatValue = Float.MAX_VALUE;
+    System.out.println("The minimun value of a float number is equal to: " + myMinimumFloatValue);
+    System.out.println("The maximun value of a float number is equal to: " + myMaximumFloatValue);
+    
+    System.out.println();
     
     double myMinimunDoubletValue = Double.MIN_VALUE;
     double myMaximunDoubletValue = Double.MAX_VALUE;
