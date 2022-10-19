fad4db0d21dc27a999d705e2d49ce91b2b4b9754
Igor Mariano <igor-mariano@outlook.com>
2022-10-19 13:44:14 -0300

    Adicionando print dos valores float

--- PrimitiveTypes/src/floatAndDouble.java
+++ PrimitiveTypes/src/floatAndDouble.java
@@ -2,4 +2,7 @@ public class floatAndDouble {
     public static void main(String[] args) throws Exception {
     float myMinimunFloatValue = Float.MIN_VALUE;
     float myMaximunFloatValue = Float.MAX_VALUE;
+    System.out.println("The minimun value of a float number is equal to: " + myMinimunFloatValue);
+    System.out.println("The maximun value of a float number is equal to: " + myMaximunFloatValue);
+    }
 }
