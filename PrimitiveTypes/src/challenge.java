cdcf788ba8c33957c7abe84a77160f321e426118
Igor Mariano <igor-mariano@outlook.com>
2022-10-19 13:21:11 -0300

    chellenge parte 1

--- PrimitiveTypes/src/challenge.java
+++ PrimitiveTypes/src/challenge.java
@@ -1,3 +1,7 @@
+/*
+ Your challenge is to create a byte variable and set it to any valid byte number, id doesn't matter.
+ Create a short variable and set it to any valid short number
+ */
 public class challenge {
     public static void main(String[] args) throws Exception {
     }
