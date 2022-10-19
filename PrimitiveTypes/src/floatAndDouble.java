7536c714a5f7f8b2a5df9c461710f7eb4abff07e
Igor Mariano <igor-mariano@outlook.com>
2022-10-19 13:41:21 -0300

    Adicionando o método main

--- PrimitiveTypes/src/floatAndDouble.java
+++ PrimitiveTypes/src/floatAndDouble.java
@@ -1,4 +1,5 @@
 public class floatAndDouble {
+    public static void main(String[] args) throws Exception {
     float myMinimunFloatValue = Float.MIN_VALUE;
     float myMaximunFloatValue = Float.MAX_VALUE;
 }
