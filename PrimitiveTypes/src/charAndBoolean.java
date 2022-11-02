47d43cb6eaef30a68c8776d0156eda4a5b552b88
Igor Mariano <igor-mariano@outlook.com>
2022-11-02 11:13:20 -0300

    Adicionando comentários sobre o tipo char

--- PrimitiveTypes/src/charAndBoolean.java
+++ PrimitiveTypes/src/charAndBoolean.java
@@ -1,6 +1,11 @@
 public class charAndBoolean {
     public static void main(String[] args) {
-
+/*
+1 - Char can store characters like a string, but one character at time and have 
+    to use simple quotes;
+2 - If we try to put more than one character we will revive a error like this message: 
+    Too many characters in character litera OR THIS Invalid character constant
+    */
         char myCahrVariable = 'C';
     }
 }
