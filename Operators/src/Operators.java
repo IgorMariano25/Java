1d0d5487341fbf50c043704886ae0d0855608a75
Igor Mariano <igor-mariano@outlook.com>
2022-11-03 15:45:43 -0300

    Pulando linha com \n

--- Operators/src/Operators.java
+++ Operators/src/Operators.java
@@ -6,14 +6,14 @@ public class Operators {
         System.out.println("====================================================");
         System.out.printf("1 + 2 = %d\n", result);
         int previsouResult = result;
-        System.out.println("previsouResult = " + previsouResult);
+        System.out.println("previsouResult = " + previsouResult + "\n");
 
         System.out.println("====================================================");
         System.out.println("                 SUBTRATION OPERATOR                ");
         System.out.println("====================================================");
         result = result - 1; // 3 - 1 = 2
         System.out.printf("3 - 1 = %d \n", result);
-        System.out.println("previsouResult = " + previsouResult);
+        System.out.println("previsouResult = " + previsouResult + "\n");
 
         /*The value of the variable previsouResult didn't change, it continuous 
         equal 3
@@ -22,7 +22,7 @@ public class Operators {
         System.out.println("                 MULTIPLY OPERATOR                  ");
         System.out.println("====================================================");
         result = result * 10;
-        System.out.printf("2 * 10 = %d \n", result);
+        System.out.println("2 * 10 = " + result + "\n");
 
         System.out.println("====================================================");
         System.out.println("                 DIVISION OPERATOR                  ");
