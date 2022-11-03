580b53b3dc88220997a50856da93e81b47bae3ac
Igor Mariano <igor-mariano@outlook.com>
2022-11-03 15:23:14 -0300

    Comentário variável result

--- Operators/src/Operators.java
+++ Operators/src/Operators.java
@@ -1,8 +1,9 @@
 public class Operators {
     public static void main(String[] args) throws Exception {
-        int result = 1 + 2;
+        int result = 1 + 2; // 1 + 2 = 3
         System.out.printf("1 + 2 = %d", result);
         int previsouResult = result;
         System.out.printf("previsouResult =" ,previsouResult);
+        result = result - 1; // 3 - 1 = 2
     }
 }
