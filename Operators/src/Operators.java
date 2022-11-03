31fa18aa5056cc050cce204f08ee58eaba91bb68
Igor Mariano <igor-mariano@outlook.com>
2022-11-03 15:20:35 -0300

    Criando a variável previous result

--- Operators/src/Operators.java
+++ Operators/src/Operators.java
@@ -2,5 +2,6 @@ public class Operators {
     public static void main(String[] args) throws Exception {
         int result = 1 + 2;
         System.out.printf("1 + 2 = %d", result);
+        int previsouResult = result;
     }
 }
