0595b454d6bc32c1730362ffde97e0f4083dbbc7
Igor Mariano <igor-mariano@outlook.com>
2022-11-03 15:31:54 -0300

    Alterando mensagem do print

--- Operators/src/Operators.java
+++ Operators/src/Operators.java
@@ -1,16 +1,16 @@
 public class Operators {
     public static void main(String[] args) throws Exception {
         int result = 1 + 2; // 1 + 2 = 3
-        System.out.printf("1 + 2 = %d", result);
+        System.out.printf("1 + 2 = %d\n", result);
         int previsouResult = result;
-        System.out.printf("previsouResult =" ,previsouResult);
+        System.out.println("previsouResult = " + previsouResult);
         result = result - 1; // 3 - 1 = 2
-        System.out.printf("3 - 1 = %d", result);
+        System.out.printf("3 - 1 = %d \n", result);
         /*The value of the variable previsouResult didn't change, it continuous 
         equal 3
         */
-        System.out.printf("previsouResult =" ,previsouResult);
+        System.out.println("previsouResult = " + previsouResult);
         result = result * 10;
-        System.out.printf("2 * 10 = %d", result);
+        System.out.printf("2 * 10 = %d \n", result);
     }
 }
