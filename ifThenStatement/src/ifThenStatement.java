eaf127ad8b9be8d516d4d0ea52fa9fcacaeec1a4
Igor Mariano <igor-mariano@outlook.com>
2022-11-06 19:37:34 -0300

    Alterando mensagem de exibição do print

--- ifThenStatement/src/ifThenStatement.java
+++ ifThenStatement/src/ifThenStatement.java
@@ -2,6 +2,6 @@ public class ifThenStatement {
     public static void main(String[] args) throws Exception {
         boolean isAlien = false;
         if (isAlien == false)
-          System.out.println("Is not a Alien");
+          System.out.println("It is not a Alien");
     }
 }
