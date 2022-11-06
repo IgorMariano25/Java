40af6f30c77c77331e9b84aea4ba01f0bfd1b2df
Igor Mariano <igor-mariano@outlook.com>
2022-11-06 13:42:19 -0300

    Estrutura básica de if

--- ifThenStatement/src/ifThenStatement.java
+++ ifThenStatement/src/ifThenStatement.java
@@ -1,5 +1,7 @@
 public class ifThenStatement {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean isAlien = false;
+            if (isAlien == false)
+            System.out.println("Is not a Alien");
     }
 }
