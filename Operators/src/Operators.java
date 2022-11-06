3f0f6dd98616f2b331ab226e7a4b3570448e4ee0
Igor Mariano <igor-mariano@outlook.com>
2022-11-06 10:03:18 -0300

    Calculando o resto da divisão da variável result

--- Operators/src/Operators.java
+++ Operators/src/Operators.java
@@ -39,5 +39,8 @@ public class Operators {
         System.out.println("====================================================");
         System.out.println("              REMAINDER OF THE DIVISION             ");
         System.out.println("====================================================");
+
+        result = result % 3; // The remainder of  ( 4 % 3 )
+
     }
 }
