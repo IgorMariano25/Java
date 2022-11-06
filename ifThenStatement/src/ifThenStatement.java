19ea0cbf1f073af7a86e2b59db2f9f0294a2124b
Igor Mariano <igor-mariano@outlook.com>
2022-11-06 20:16:47 -0300

    Estrutura if com operador lógico && (AND)

--- ifThenStatement/src/ifThenStatement.java
+++ ifThenStatement/src/ifThenStatement.java
@@ -9,5 +9,10 @@ public class ifThenStatement {
         if (topScore == 100) {
             System.out.println("You got the high score!");
         }
+
+        int secondScore = 81;
+        if (topScore > secondScore && topScore < 100){
+            System.out.println("Greater than second top score and less than 100");
+        }
     }
 }
