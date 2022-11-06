80d0d733407f5337dcdfaf3ff86a740af0fa72d6
Igor Mariano <igor-mariano@outlook.com>
2022-11-06 19:55:34 -0300

    Criando bloco de código

--- ifThenStatement/src/ifThenStatement.java
+++ ifThenStatement/src/ifThenStatement.java
@@ -1,7 +1,8 @@
 public class ifThenStatement {
     public static void main(String[] args) throws Exception {
         boolean isAlien = false;
-        if (isAlien == false)
+        if (isAlien == false){
           System.out.println("It is not a Alien");
+        }
     }
 }
