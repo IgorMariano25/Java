49cfa5dfc33bdf1c4823d69f589fcc43aa7a5e7f
Igor Mariano <igor-mariano@outlook.com>
2022-12-02 10:14:37 -0300

    Criando a variável highScore

--- KeywordsAndExpressions/src/keywordsAndExpressions.java
+++ KeywordsAndExpressions/src/keywordsAndExpressions.java
@@ -1,5 +1,7 @@
 public class keywordsAndExpressions {
     public static void main(String[] args) throws Exception {
         double Kilometers = (100 * 1.609344);
+
+        int highScore = 50;
     }
 }
