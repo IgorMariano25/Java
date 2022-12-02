e9007fd3d2a88c16e24925ea7df00b3f3a257244
Igor Mariano <igor-mariano@outlook.com>
2022-12-02 11:38:06 -0300

    Adicionando print para a variável Kilometers

--- KeywordsAndExpressions/src/keywordsAndExpressions.java
+++ KeywordsAndExpressions/src/keywordsAndExpressions.java
@@ -1,6 +1,7 @@
 public class keywordsAndExpressions {
     public static void main(String[] args) throws Exception {
         double Kilometers = (100 * 1.609344);
+        System.out.print("100 Miles is equal to: " + Math.round(Kilometers) + " Km");
 
         int highScore = 50;
 
